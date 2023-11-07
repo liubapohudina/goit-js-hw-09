c7a2c5b01c05806fcc4dcbd0a24251f3cfadb8bd
Liubov <drogaltseva92@gmail.com>
2023-11-07 17:54:40 +0100

    create css

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+btnStop.disabled = true
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
 
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+btnStop.addEventListener("click", () => {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+  btnStop.disabled = truestar;
+});
---
+++ src/js/02-timer.js
@@ -0,0 +1,3 @@
+const inputDate = document.querySelector('#datetime-picker')
+const newDate = inputDateclecl
+console.log(inputDate)
