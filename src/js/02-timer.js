525c1f34752e8aae444844c5399b3a37f59ab064
Liubov <drogaltseva92@gmail.com>
2023-11-13 12:28:16 +0100

    add flat

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,7 @@
-const inputDate = document.querySelector('#datetime-picker')
-const newDate = inputDateclecl
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+const inputDate = flatpickr("#datetime-picker")
+const buttonStart = document.querySelector("button[data-start]")
+const currentDate = new Date()
+inputDate.textContent = currentDate
 console.log(inputDate)
