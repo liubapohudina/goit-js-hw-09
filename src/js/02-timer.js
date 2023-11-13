13bde51a86c7a8451c69797af0041e81cd4b2bc3
Liubov <drogaltseva92@gmail.com>
2023-11-13 12:32:44 +0100

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,11 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 const inputDate = flatpickr("#datetime-picker")
 const buttonStart = document.querySelector("button[data-start]")
-const currentDate = new Date()
-inputDate.textContent = currentDate
-console.log(inputDate)
+const day = document.querySelector('span[data-days]')
+const hour = document.querySelector('span[data-hours]')
+const minute = document.querySelector('span[data-minutes]')
+const second = document.querySelector('span[data-seconds]')
+//const currentDate = new Date()
+//inputDate.textContent = currentDate
+//console.log(inputDate)
+buttonStart.disabled = true
