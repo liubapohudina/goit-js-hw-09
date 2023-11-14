7e42bdc464c89a35af8a55f80034ece79c8112cb
liubapohudina <116346379+liubapohudina@users.noreply.github.com>
2023-11-14 08:44:00 +0000

    Deploying to gh-pages from @ liubapohudina/goit-js-hw-09@4331e67bdb8650445126f9f188dacfec51b007a1 🚀

---
+++ 01-color-switcher.02c3b24d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");document.querySelector("body");e.disabled=!0;var d=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,d=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.02c3b24d.js.map
